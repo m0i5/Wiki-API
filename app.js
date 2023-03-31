04d5fb67adc0a880c58ceb466b48bae9a7a671d6
m0i5 <111139099+m0i5@users.noreply.github.com>
2023-03-31 15:21:20 +0200

    GET, POST, DELETE Articles

--- app.js
+++ app.js
@@ -7,24 +7,50 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+// setting 'ejs' as templating engine
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+// using public directory to store static files such as images and css code
 app.use(express.static("public"));
+
+
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
 const articleSchema = {
     title: String,
-    conent: String
+    content: String
 }
 
 const Article = mongoose.model("Article", articleSchema);
 
 
-//TODO
+
+app.get("/articles", function(req,res){
+Article.find({}).then(foundArticles => {
+  res.send(foundArticles);
+}).catch(err => console.log(err));
+
+;})
+
+//post-request targets the 'articles' route, then creates a 'newArticle' 
+app.post("/articles", async (req, res) => {
+  const newArticle = new Article ({
+      title: req.body.title,
+      content: req.body.content
+  })
+  newArticle.save().then(success => {  //send it to the client when success
+      res.send("Succesfully added a new article");
+  }).catch(err => { //catch error and send it
+      res.send(err);
+  })
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
